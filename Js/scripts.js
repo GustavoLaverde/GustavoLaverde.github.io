f19b1396c79b3a6b70485a5fadfab47249c81e81
Gustavo Laverde <gustavo.laverde.a@uni.pe>
2021-03-27 23:46:27 -0500

    reparo de un bugv2
    reparo de un bug a media v2

--- Js/scripts.js
+++ Js/scripts.js
@@ -5,8 +5,7 @@ const ipad = window.matchMedia('screen and (max-width: 767px)');
         const burgerButton = document.querySelector("#burger-menu");
         // console.log(burgerButton);
         
-        // ipad.addListener(validation)
-        
+        validation();        
         function validation(event){
             console.log(event.matches);
             if (event.matches){
