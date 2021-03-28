cd7b8f4ebb981a7781311b86b50959b494468a58
Gustavo Laverde <gustavo.laverde.a@uni.pe>
2021-03-27 23:28:04 -0500

    primer commit
    Primer Commit  27 de marzo del 2021

---
+++ Js/scripts.js
@@ -0,0 +1,25 @@
+const ipad = window.matchMedia('screen and (max-width: 767px)');
+        // console.log("hola mundo");
+        const menu = document.querySelector(".menu");
+        // console.log(menu);
+        const burgerButton = document.querySelector("#burger-menu");
+        // console.log(burgerButton);
+        
+        ipad.addListener(validation)
+        
+        function validation(event){
+            console.log(event.matches);
+            if (event.matches){
+                burgerButton.addEventListener('click', hideShow);
+            }else{
+                burgerButton.removeEventListener('click', hideShow);
+            }
+        }
+
+        function hideShow(){
+            if (menu.classList.contains("is-active")){
+                menu.classList.remove("is-active");
+            }else{
+                menu.classList.add("is-active");
+            }
+        }
